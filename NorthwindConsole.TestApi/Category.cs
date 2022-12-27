258c4b181fe771a4020111011e0ac7cb995ca919
edanurktly <kutluayedanur@gmail.com>
2022-12-27 20:20:53 +0300

    Add project files.

---
+++ NorthwindConsole.TestApi/Category.cs
@@ -0,0 +1,9 @@
+﻿namespace NorthwindConsole.TestApi
+{
+    public class Category
+    {
+        public int Id { get; set; }
+        public string Description { get; set; }
+        public string Name { get; set; }
+    }
+}
